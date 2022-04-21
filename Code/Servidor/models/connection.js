7faa236045f5eadebece04044f89be6bc53ece27
filomeno merlath <filo.meno.junior@hotmail.com>
2022-04-21 02:10:55 +0100

    database

--- Code/Servidor/models/connection.js
+++ Code/Servidor/models/connection.js
@@ -1,15 +1,15 @@
 var pg = require("pg");
 
-//const connectionString ="postgres://bzvfvvqu:hpNSZKZrOBiAlCdQn0_b4I0z_pknn8_d@surus.db.elephantsql.com/bzvfvvqu";
-const connectionString = "postgres://postgres:junior@localhost:5432/streetart";
+const connectionString =
+  "postgres://yjrebhnm:vzeYWKe7vGpHpLhNbE_2f_k4mYKLeEO-@rogue.db.elephantsql.com/yjrebhnm";
 const Pool = pg.Pool;
 const pool = new Pool({
   connectionString,
   max: 10,
-  /* ssl: {
+  ssl: {
     require: true,
     rejectUnauthorized: false,
-  },*/
+  },
 });
 
 module.exports = pool;
