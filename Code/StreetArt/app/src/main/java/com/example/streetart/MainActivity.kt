6f194f2b73f73401be31156034850d0d5c1b4154
William Fejó <dkamino53@gmail.com>
2022-04-26 14:41:30 +0100

    update apk

--- Code/StreetArt/app/src/main/java/com/example/streetart/MainActivity.kt
+++ Code/StreetArt/app/src/main/java/com/example/streetart/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.streetart
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.Paint
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    private var mapboxMap: MapboxMap? = null
+    private var permissionsManager: PermissionsManager? = null
+    private var locationComponent: LocationComponent? = null
     private var mapView: MapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +48,58 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    /*private fun enableLocationComponent(loadedMapStyle: Style?){
+        if(PermissionsManager.areLocationPermissionsGranted(this)){
+            locationComponent = mapboxMap!!.locationComponent
+            locationComponent!!.activateLocationComponent(this, loadedMapStyle!!)
+            locationComponent!!.setLocationComponentEnabled(true)
+
+            locationComponent!!.setCameraMode(CameraMode.TRACKING)
+        }else{
+            permissionsManager = PermissionsManager(this)
+            permissionsManager!!.requestLocationPermissions(this)
+        }
+    }*/
+
+   /* private fun onExplanationNeeded(permissionsToExplain: MutableList<String?>){
+        Toast.makeText(this, R.string.user_location_permission_explanation, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onPermissionResult(granted: Boolean){
+        if(granted){
+            enableLocationComponent(mapboxMap!!.style)
+        }else{
+            Toast.makeText(this, R.string.user_location_permission_not_granted, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }*/
+
+   /* private fun requestPermissionLauncher(){
+        requestPermission(Manifest.permission.CAMERA)
+    }
+
+    private fun requestPermission(permission: String){
+        when{
+            ContextCompat.checkSelfPermission(
+                    this,
+                    permission
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("Kilo", "Permission previously granted")
+            }
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    permission
+            ) ->
+                Log.i("Kilo", "Show camera permissions dialog")
+            else ->
+                requestPermissionLauncher.launch(permission)
+        }
+    }*/
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
@@ -61,4 +125,6 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         mapView?.onDestroy()
     }
+
+
 }
---
+++ Code/StreetArt/app/src/main/java/com/example/streetart/PermissionsManager.kt
@@ -0,0 +1,32 @@
+package com.example.streetart
+
+class PermissionsManager {
+  lateinit var permissionsManager: PermissionsManager
+}
+/*class PermissionsManager {
+    var permissionsManager: `var`? = null
+    var `fun`: override? = null
+    fun onRequestPermissionsResult() {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    var permissionsListener: `var`? = null
+    fun PermissionsListener() {
+        var `fun`: override
+        onExplanationNeeded(permissionsToExplain)
+        run {}
+        var `fun`: override
+        onPermissionResult(granted)
+        Boolean
+        run {
+            if (granted) {
+            } else {
+            }
+        }
+    }
+
+    init {
+        permissionsManager = PermissionsManager(this)
+        permissionsManager.requestLocationPermissions(this)
+    }
+}*/
