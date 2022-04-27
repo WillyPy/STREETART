0a3f479fd8b63bf6a284d9a898cf1ace147c3f1b
William Fejó <dkamino53@gmail.com>
2022-04-27 15:54:29 +0100

    Update Map

--- Code/StreetArt/app/src/main/java/com/example/streetart/MainActivity.kt
+++ Code/StreetArt/app/src/main/java/com/example/streetart/MainActivity.kt
@@ -1,46 +1,111 @@
 package com.example.streetart
 
-import android.graphics.Paint
+import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.streetart.databinding.ActivityMainBinding
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
-import android.util.Log
-import com.example.streetart.api.RetrofitHelper
-import com.example.streetart.api.streetartApi
-
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import retrofit2.create
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.gestures.gestures
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
-    private var mapView: MapView? = null
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+    private /*leteinit*/var mapView: MapView? = null
+    private val onIndicatorBearingChangedListener =
+        OnIndicatorBearingChangedListener{
+        mapView?.getMapboxMap()?.setCamera(CameraOptions.Builder().bearing(it).build())
+    }
+    private val onIndicatorPositionChangedListener=
+        OnIndicatorPositionChangedListener{
+            mapView?.getMapboxMap()?.setCamera(CameraOptions.Builder().center(it).build())
+            mapView?.gestures?.focalPoint=
+                mapView?.getMapboxMap()?.pixelForCoordinate(it)
+        }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ){ isGranted ->
+        if(isGranted){
+            Log.i("Test", "Permission granted")
+        } else
+            Log.i("Test", "Permission denied")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_second)
         setSupportActionBar(findViewById(R.id.toolbar))
-        mapView= findViewById(R.id.mapView)
+        mapView= findViewById(com.mapbox.maps.R.id.mapView)
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
-        val quotesApi = RetrofitHelper.getInstance().create(streetartApi::class.java)
-        // launching a new coroutine
-        GlobalScope.launch {
-            val result = quotesApi.getUsers()
-            if (result != null)
-            // Checking the results
-                Log.d("users: ", result.body().toString())
-        }
+
     }
 
+    private fun initLocationComponent(){
+        val locationComponentPlugin= mapView?.location
+        locationComponentPlugin?.updateSettings {
+            this.enabled = true
+            this.locationPuck = LocationPuck2D(
+                bearingImage = AppCompatResources.getDrawable(
+                    this@MainActivity,
+                    com.mapbox.maps.R.drawable.mapbox_user_puck_icon
+                ),
+                shadowImage = AppCompatResources.getDrawable(
+                    this@MainActivity,
+                    com.mapbox.maps.R.drawable.mapbox_user_icon_shadow
+                ),
+                scaleExpression = interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(0.0)
+                        literal(0.6)
+                    }
+                    stop {
+                        literal(20.0)
+                        literal(1.0)
+                    }
+                }.toJson()
+            )
+        }
+        locationComponentPlugin?.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        locationComponentPlugin?.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+    }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -54,6 +119,34 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+   /* private fun requestPermissionLauncher(){
+        requestPermission(Manifest.permission.CAMERA)
+    }*/
+    private fun requestPermission(permission: String){
+        when{
+            ContextCompat.checkSelfPermission(
+                this,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.i("Test", "Permission previously granted")
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                permission
+            ) ->
+                Log.i("Test", "Show camera permissions dialog")
+            else ->
+                requestPermissionLauncher.launch(permission)
         }
     }
 
