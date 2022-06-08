6eb863ca63d8d4afb600b0c75619c6d36fa4b4c4
filomeno merlath <filo.meno.junior@hotmail.com>
2022-06-08 15:44:05 +0100

    all commit

--- Code/Server/public/javascripts/art.js
+++ Code/Server/public/javascripts/art.js
@@ -1,25 +1,22 @@
+var mymap;
+var aId = sessionStorage.getItem("artId");
 window.onload = async function () {
-  loadArt(sessionStorage.getItem("artId"));
+  loadArt();
 };
-
 async function loadArt(aId) {
   try {
     console.log(aId);
-    let art = await $.ajax({
-      url: `/api/arts/${aId}`,
-      method: `get`,
-      dataType: `json`,
-    });
     let images = await $.ajax({
       url: `/api/arts/${aId}/images`,
       method: `get`,
       dataType: `json`,
     });
+
     let html = `<section id="arts"> `;
     for (let img of images)
       html += `<img class="artImgs" src="${img.image_link}">`;
     html += "</section>";
-    document.getElementById("art").innerHTML = html;
+    document.getElementById("conteiner").innerHTML = html;
   } catch (error) {
     console.log(error);
   }
